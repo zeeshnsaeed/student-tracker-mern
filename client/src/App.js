3e4dbf258c00c9075b1f2c6918413152d5ef4426
Zeeshan Saeed <zsaeed830@gmail.com>
2024-01-01 00:29:27 +0500

    client setup completed

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function App() {
+  return (
+    <>
+      <div>Hello, World!</div>
+    </>
+  );
+}
+
+export default App;
