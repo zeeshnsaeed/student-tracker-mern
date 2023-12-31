55b90f883c8b8886dea8da425ef9ec1428ebbb9f
Zeeshan Saeed <zsaeed830@gmail.com>
2024-01-01 01:27:53 +0500

    feat: navbar completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
     <>
-      <div>Hello, World!</div>
+      <NavBar />
+      {/* <div>Hello, Zeeshan</div> */}
     </>
   );
 }
